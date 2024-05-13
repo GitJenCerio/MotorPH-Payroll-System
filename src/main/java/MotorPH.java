0b2870b6c0bf394b6713e887d2b8dd14bd5b8898
Processing Dept <Processing Dept@PROCESSING-JHEN>
2024-05-13 16:37:18 +0800

    First Commit

---
+++ src/main/java/MotorPH.java
@@ -0,0 +1,14 @@
+
+import UI.MainUI;
+public class MotorPH {
+
+    public static void main(String[] args) {
+       
+                MainUI mainUI = new MainUI();
+                // Set the frame visible
+                mainUI.setVisible(true);
+            }
+       
+    }
+
+
